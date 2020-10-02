1f8e97e49f36787ddc57a86d0810bd8984cfedd6
lianming <you@example.com>
2020-10-03 07:07:13 +0800

    show kttree

--- hello-world-min/App.js
+++ hello-world-min/App.js
@@ -8,10 +8,14 @@ window.THREE = THREE;
 
 class App {
   constructor() {
+    this.root = new THREE.Group();
+    this.root.name = "draw";
     window.lm = this;
     this.stage = new Stage("#app");
     this.stage.run();
-    this.addPoint();
+    this.stage.scene.add(this.root);
+
+    // this.addPoint();
     // 添加范围线
     this.addLine({ x: 10, y: 0 }, { x: 10, y: 10 }, 0x000000);
     this.addLine({ x: 0, y: 10 }, { x: 10, y: 10 }, 0x000000);
@@ -24,7 +28,11 @@ class App {
   }
 
   setCamera() {
-    let cam = {x: 4.822139751357829, y: 4.044927434060573, z: 26.6408009313641}
+    let cam = {
+      x: 4.822139751357829,
+      y: 4.044927434060573,
+      z: 26.6408009313641,
+    };
     this.stage.camera.position.set(cam.x, cam.y, cam.z);
 
     let t = {
@@ -39,8 +47,8 @@ class App {
     // 生成坐标轴图片 https://www.tutorialsteacher.com/codeeditor?cid=d3-46
     let box = new THREE.BoxGeometry(10, 1, 1);
     let texture = await loadTexture("./axis.png");
-    texture.magFilter = THREE.NearestFilter
-    texture.minFilter = THREE.NearestFilter
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
     let mat = new THREE.MeshBasicMaterial({
       map: texture,
       color: 0xffbbbb,
@@ -57,8 +65,8 @@ class App {
     // 生成坐标轴图片 https://www.tutorialsteacher.com/codeeditor?cid=d3-46
     let box = new THREE.BoxGeometry(10, 1, 1);
     let texture = await loadTexture("./axis.png");
-    texture.magFilter = THREE.NearestFilter
-    texture.minFilter = THREE.NearestFilter
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
 
     let mat = new THREE.MeshBasicMaterial({
       map: texture,
@@ -72,16 +80,30 @@ class App {
     this.stage.scene.add(cube);
   }
 
-  addLine(s, e, color=0x0000ff) {
+  addLine(s, e, color = 0x0000ff) {
     let l = new Line({
       start: { x: s.x, y: s.y, z: 0 },
       end: { x: e.x, y: e.y, z: 0 },
-      color
+      color,
     });
-    this.stage.scene.add(l.getRoot());
+    // this.stage.scene.add(l.getRoot());
+    l.getRoot().name = "line_" + new Date().getTime();
+    if (color == 0x000000) {
+      scene.add(l.getRoot());
+    } else {
+      this.root.add(l.getRoot());
+    }
   }
 
-  addBox(pos) {
+  claer() {
+    scene.remove(this.root);
+    this.root = null;
+    this.root = new THREE.Group();
+    this.root.name = "draw";
+    scene.add(this.root);
+  }
+
+  addPoint(pos) {
     var geometry = new THREE.SphereGeometry(0.1, 64, 64);
     var material = new THREE.MeshBasicMaterial({
       color: 0x000000,
@@ -89,23 +111,24 @@ class App {
     });
     var cube = new THREE.Mesh(geometry, material);
     cube.position.copy(pos);
-    cube.name = "test_cube";
-    this.stage.scene.add(cube);
+    cube.name = "point_" + new Date().getTime();
+    // this.root.add(cube);
+    scene.add(cube);
   }
 
-  addPoint() {
-    let arr = [
-      { x: 2, y: 3 },
-      { x: 5, y: 4 },
-      { x: 9, y: 6 },
-      { x: 4, y: 7 },
-      { x: 8, y: 1 },
-      { x: 7, y: 2 },
-    ];
-    arr.forEach((item) => {
-      this.addBox(new THREE.Vector3(item.x, item.y, item.z));
-    });
-  }
+  // addPoint() {
+  //   let arr = [
+  //     { x: 2, y: 3 },
+  //     { x: 5, y: 4 },
+  //     { x: 9, y: 6 },
+  //     { x: 4, y: 7 },
+  //     { x: 8, y: 1 },
+  //     { x: 7, y: 2 },
+  //   ];
+  //   arr.forEach((item) => {
+  //     this.addBox(new THREE.Vector3(item.x, item.y, item.z));
+  //   });
+  // }
 }
 
 window.onload = () => {
--- hello-world-min/TreeApp.js
+++ hello-world-min/TreeApp.js
@@ -1,4 +1,5 @@
 import KDTree from "./kdTree.js";
+import { sleep } from "./utils.js";
 
 class Point {
   constructor(x, y) {
@@ -11,38 +12,37 @@ class Point {
 export default class TreeApp {
   constructor(draw) {
     this.points = [];
-    this.width = 10
-    this.height = 10
+    this.width = 10;
+    this.height = 10;
     this.tree = new KDTree("median");
     window.tree = this.tree;
-    this.draw = draw
+    this.draw = draw;
     this.loadData();
   }
 
-  redraw(){
-    if(this.points.length == 6) {
-      this.drawDividingLines(this.tree.root)
-    }
+  redraw() {
+    this.draw.claer();
+    this.drawDividingLines(this.tree.root);
   }
 
   calcViewPort(node) {
-    let s = { x: 0, y: 0,z:0 };
-    let e = { x: 0, y: 0,z:0 };
-    s = new THREE.Vector3(s.x, s.y,s.z)
-    e = new THREE.Vector3(e.x, e.y,e.z)
+    let s = { x: 0, y: 0, z: 0 };
+    let e = { x: 0, y: 0, z: 0 };
+    s = new THREE.Vector3(s.x, s.y, s.z);
+    e = new THREE.Vector3(e.x, e.y, e.z);
 
     // 根节点
     if (!node.parent) {
       if (node.isAxisX()) {
         s.x = node.point.x;
         s.y = 0;
-  
+
         e.x = node.point.x;
         e.y = this.height;
       } else {
         s.x = 0;
         s.y = node.point.y;
-  
+
         e.x = this.width;
         e.y = node.point.y;
       }
@@ -52,31 +52,31 @@ export default class TreeApp {
       // console.error(dir)
       if (node.isAxisX()) {
         // 垂直方向
-        if(dir == "left") {
+        if (dir == "left") {
           s.x = node.point.x;
           s.y = 0;
-    
+
           e.x = node.point.x;
           e.y = parentNode.point.y;
         } else {
           s.x = node.point.x;
           s.y = parentNode.point.y;
-    
+
           e.x = node.point.x;
-          e.y = this.height
+          e.y = this.height;
         }
       } else {
         // 水平方向
         if (dir == "left") {
           s.x = 0;
           s.y = node.point.y;
-  
+
           e.x = parentNode.point.x;
           e.y = node.point.y;
         } else if (dir == "right") {
           s.x = parentNode.point.x;
           s.y = node.point.y;
-  
+
           e.x = this.width;
           e.y = node.point.y;
         }
@@ -84,10 +84,9 @@ export default class TreeApp {
     }
     return { start: s, end: e };
   }
-  
-  drawDividingLines(node) {
+
+  async drawDividingLines(node) {
     if (node === null) return;
-  
     // x方向颜色
     let blue = 0x0000ff;
     // y方向颜色
@@ -102,7 +101,8 @@ export default class TreeApp {
 
   recalculateTree() {
     this.tree.init(this.points);
-    this.redraw()
+
+    this.redraw();
   }
 
   addPoint(point) {
@@ -116,7 +116,7 @@ export default class TreeApp {
     }
   }
 
-  loadData() {
+  async loadData() {
     let arr = [
       { x: 2, y: 3 },
       { x: 5, y: 4 },
@@ -125,9 +125,11 @@ export default class TreeApp {
       { x: 8, y: 1 },
       { x: 7, y: 2 },
     ];
-
-    arr.forEach((item) => {
+    for (let i = 0; i < arr.length; i++) {
+      let item = arr[i];
+      await sleep(2000);
+      this.draw.addPoint(new THREE.Vector3(item.x, item.y, item.z));
       this.addPoint(new Point(item.x, item.y));
-    });
+    }
   }
 }
